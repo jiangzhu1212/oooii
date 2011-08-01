9a43f01b3ee8d04854e9b54cd3b3de634a0ae168
arciuolo <arciuolo@2228257d-c0e2-8e59-bf8f-98304f4dbe51>
2011-08-01 05:13:41 +0000

    Refactor stuff so it doesn't crash - has RenderTarget resizing well.

--- trunk/oGfx/oGfxTest.cpp
+++ trunk/oGfx/oGfxTest.cpp
@@ -8,9 +8,24 @@
 
 #include <oGfx/oGfx.h>
 
-int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+struct oApp
 {
+	oApp();
+	void Run();
+
+protected:
+	void OnResize(oWindow::RECT_EVENT _Event, oWindow::STATE _State, oRECT _Rect);
+
 	oRef<oWindow> Window;
+	oRef<threadsafe oKeyboard> Keyboard;
+	oRef<threadsafe oMouse> Mouse;
+	oRef<threadsafe oGfxDevice> Device;
+	oRef<threadsafe oWindow::Resizer> Resizer;
+	oRef<oGfxRenderTarget2> RenderTarget;
+};
+
+oApp::oApp()
+{
 	{
 		// @oooii-tony: If X is specified, but Y is intended to be default,
 		// then bad things happen. We should find out why.
@@ -19,40 +34,62 @@ int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int n
 		d.ClientY = 200;
 		d.ClientWidth = 800;
 		d.ClientHeight = 600;
-		d.Style = oWindow::FIXED;
+		d.Style = oWindow::SIZEABLE;
 		d.UseAntialiasing = true;
 		oVERIFY(oWindow::Create(&d, nullptr, "oGfxTest", 0, &Window));
 	}
 
-	oRef<threadsafe oKeyboard> Keyboard;
 	oVERIFY(oKeyboard::Create(Window->GetNativeHandle(), true, &Keyboard));
 
-	oRef<threadsafe oMouse> Mouse;
 	{
 		oMouse::DESC d;
 		d.ShortCircuitEvents = false;
 		oVERIFY(oMouse::Create(d, Window->GetNativeHandle(), &Mouse));
 	}
 
-	Mouse->SetCursorState(oMouse::NORMAL);
-
-	oRef<threadsafe oGfxDevice> GfxDevice;
 	{
 		oGfxDevice::DESC d;
 		d.EnableDebugReporting = true;
 		d.UseSoftwareEmulation = false;
 		d.Version = 10.0f;
-		oVERIFY(oGfxCreateDevice(d, &GfxDevice));
+		oVERIFY(oGfxCreateDevice(d, &Device));
 	}
 
-	oRef<oGfxRenderTarget2> RenderTarget;
-	oVERIFY(GfxDevice->CreateRenderTarget2("Output", Window, oSurface::R32_TYPELESS, &RenderTarget));
+	oVERIFY(Window->CreateResizer(oBIND(&oApp::OnResize, this, oBIND1, oBIND2, oBIND3), &Resizer));
 
+	//oVERIFY(Device->CreateRenderTarget2("Output", Window, oSurface::R32_TYPELESS, &RenderTarget));
+
+	Mouse->SetCursorState(oMouse::NORMAL);
+}
+
+void oApp::OnResize(oWindow::RECT_EVENT _Event, oWindow::STATE _State, oRECT _Rect)
+{
+	if (_State == oWindow::RESTORED || _State == oWindow::FULL_SCREEN)
+	{
+		if (_Event == oWindow::RESIZE_OCCURING)
+		{
+			RenderTarget = nullptr;
+		}
+
+		else if (_Event == oWindow::RECT_END)
+		{
+			oVERIFY(Device->CreateRenderTarget2("Output", Window, oSurface::R32_TYPELESS, &RenderTarget));
+		}
+	}
+}
+
+void oApp::Run()
+{
 	while (Window->IsOpen())
 	{
 		Window->Begin();
 		Window->End();
 	}
-	
+}
+
+int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	oApp app;
+	app.Run();
 	return 0;
 }
