457d67b15037ac2f5abfbe455cd5e8c48d310e8c
arciuolo <arciuolo@2228257d-c0e2-8e59-bf8f-98304f4dbe51>
2011-08-01 01:38:16 +0000

    Create a blank window mouse and kb

--- trunk/oGfx/oGfxTest.cpp
+++ trunk/oGfx/oGfxTest.cpp
@@ -4,9 +4,40 @@
 
 #include <oooii/oMsgBox.h>
 
+#include <oooii/oKeyboard.h>
+#include <oooii/oMouse.h>
+#include <oooii/oWindow.h>
+
+
 int WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
-	oMsgBox::printf(oMsgBox::INFO, "oGfxText", "Hello World!");
+	oRef<oWindow> Window;
+	{
+		oWindow::DESC d;
+		d.ClientWidth = 800;
+		d.ClientHeight = 600;
+		d.Style = oWindow::FIXED;
+		d.UseAntialiasing = true;
+		oVERIFY(oWindow::Create(&d, nullptr, "oGfxTest", 0, &Window));
+	}
 
+	oRef<threadsafe oKeyboard> Keyboard;
+	oVERIFY(oKeyboard::Create(Window->GetNativeHandle(), true, &Keyboard));
+
+	oRef<threadsafe oMouse> Mouse;
+	{
+		oMouse::DESC d;
+		d.ShortCircuitEvents = false;
+		oVERIFY(oMouse::Create(d, Window->GetNativeHandle(), &Mouse));
+	}
+
+	Mouse->SetCursorState(oMouse::NORMAL);
+
+	while (Window->IsOpen())
+	{
+		Window->Begin();
+		Window->End();
+	}
+	
 	return 0;
 }
