33174fb1196f73fa1a5d9b7af11b5926b452feb9
arciuolo <arciuolo@2228257d-c0e2-8e59-bf8f-98304f4dbe51>
2012-10-17 05:20:06 +0000

    Remove ext as part of removing all contents. A very different tree structure is coming, so clear the way.

--- trunk/ext/vp8/build/vpx_config.c
+++
@@ -1,2 +0,0 @@
-static const char* const cfg = "--enable-debug-libs --enable-codec-srcs --enable-install-docs --enable-install-srcs --enable-static-msvcrt --target=x86-win32-vs9";
-const char *vpx_codec_build_config(void) {return cfg;}
