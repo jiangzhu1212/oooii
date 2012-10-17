6bfadab64a5aa99acc56c82c0a0deb049d78bede
arciuolo <arciuolo@2228257d-c0e2-8e59-bf8f-98304f4dbe51>
2012-10-17 05:20:50 +0000

    Remove the entire tree as part of removing all contents. A very different tree structure is coming, so clear the way.

--- trunk/MSSupport/oP4Edit.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-p4 edit "%1"
-set EXT=%~x1
-if "%EXT%"==".vcxproj" p4 edit "%1.filters"
--- trunk/MSSupport/oP4Revert.cmd
+++
@@ -1,4 +0,0 @@
-@echo off
-p4 revert "%1"
-set EXT=%~x1
-if "%EXT%"==".vcxproj" p4 revert "%1.filters"
